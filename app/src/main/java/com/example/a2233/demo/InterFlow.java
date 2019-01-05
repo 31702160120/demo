da5e17f64789a4cb66006e775087f4d9a8cdf53f
31702160120 <860079735@qq.com>
2019-01-05 12:33:59 +0800

    no message

---
+++ app/src/main/java/com/example/a2233/demo/InterFlow.java
@@ -0,0 +1,13 @@
+package com.example.a2233.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InterFlow extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inter_flow);
+    }
+}
